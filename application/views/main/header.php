725fcd9ed70e7ac5fc34683bbee17fb8faf0cb16
Richard Robinson <kincade71@gmail.com>
2019-10-09 18:13:24 -0400

    Added view template

---
+++ application/views/main/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>
+
+        </title>
+        <!-- CSS -->
+        <link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- End CSS -->
+        <!-- Favicons and touch icons -->
+        <link rel="shortcut icon" href="<?= base_url()?>img/favicon.ico">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= base_url()?>img/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= base_url()?>img/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= base_url()?>img/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= base_url()?>img/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= base_url()?>img/apple-touch-icon-57x57.png">
+        <!-- End Favicons and touch icons -->
+    </head>
+    <body>
--- index.php
+++ index.php
@@ -96,8 +96,7 @@ switch (ENVIRONMENT)
  *
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
- * 	vendor/bootstrap/dist/css/bootstrap.min.css
- * vendor/bootstrap/dist/js/bootstrap.min.js
+ * 	
  */
 	$system_path = 'vendor/webdmg/system-c/system';
 
